f2b2c84445bce5325abd1383c8cd1313a61b4912
Avaricia <94759980+AvariciaTZ@users.noreply.github.com>
2022-06-19 20:01:20 -0500

    PedroDavid-actualizar codigo IDENTIFICADOR
    PedroDavid-actualizar codigo IDENTIFICADOR

--- team/PedroDavid.php
+++ team/PedroDavid.php
@@ -1,41 +1,58 @@
 <?php
-/* IDENTIFICADOR: VARIABLE */
-//length_Num_=(2+4)/3 if (_Num_>10) then 'Ingresaste'";
+/* var_dump(Identificador($evaluar)); */
 
-
-/* $buscado = explode(" ", $evaluar); */
-//echo Identificador($buscado, $sql, $msg);
-/* $evaluar = " _nag = ( 2 + 4 ) / 3 if ( _Num > 10 ) then ' Ingresaste '";
-echo var_dump(Identificador($evaluar)); */
-/* Lógica */
 function Identificador($evaluar)
 {
-    // $evaluar = " _nag = ( 2 + 4 ) / 3 if ( _Num > 10 ) then ' Ingresaste '";
-    /* letra por letra */
-    /*  $buscado = str_split($evaluar); */
-
-    $data =  array();
-    $mayusculas =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    $mayusculas =  ['_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     $minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    /* $ignorar =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; */
+
     $operators = array_merge($minusculas, $mayusculas);
 
     /* analisame todo el array que debe coincidir por letra */
 
-    foreach ($operators as $operator) {
-        /* me da la position del que coincide*/
-        $position  =  strpos($evaluar, $operator);
-        /* si se encuentra una valor identico es true */
-        if ($position !== false) {
-            /* este */
-            /* var_dump($position); */
-            /*  var_dump($operator); */
-            /*   $alm[$position] =  array($operator, 'identificador', $operator); */
-            if (!is_null($operator)) {
-                $data[] = array($operator, 'identificador', $position);
+    $bus = str_split($evaluar);
+    $alm =  array();
+    $alm[] = preg_replace("/[[:space:]]/", "", trim($evaluar));
+
+    $data =  array();
+    $datosAlmacenados =  array();
+
+    for ($i = 0; $i < count($operators); $i++) {
+        /*  */
+        if ($bus[$i] == '_') {
+            /* array_push($datosAlmacenados, "<br/>" . $bus[$i]); */
+            $datosAlmacenados[] = array("<br/>" . $bus[$i], 'identificador', $i);
+            if ($bus[$i + 1] !== ' ') {
+                /* array_push($datosAlmacenados, $bus[$i + 1]); */
+                $datosAlmacenados[] = array($bus[$i + 1], 'identificador', $i);
+                if ($bus[$i + 2] !== ' ') {
+                    /* array_push($datosAlmacenados, $bus[$i + 2]); */
+                    $datosAlmacenados[] = array($bus[$i + 2], 'identificador', $i);
+                    if ($bus[$i + 3] !== ' ') {
+                        /* array_push($datosAlmacenados, $bus[$i + 3]); */
+                        $datosAlmacenados[] = array($bus[$i + 3], 'identificador', $i);
+                        if ($bus[$i + 4] !== ' ') {
+                            /* array_push($datosAlmacenados, $bus[$i + 4]); */
+                            $datosAlmacenados[] = array($bus[$i + 4], 'identificador', $i);
+                            if ($bus[$i + 5] !== ' ') {
+                                /*   array_push($datosAlmacenados, $bus[$i + 5]); */
+                                $datosAlmacenados[] = array($bus[$i + 5], 'identificador', $i);
+                                if ($bus[$i + 6] !== ' ') {
+                                    /*   array_push($datosAlmacenados, $bus[$i + 6]); */
+                                    /*  $datosAlmacenados[] = array($bus[$i + 6], 'identificador', $i); */
+                                    $datosAlmacenados[] = array($bus[$i + 6], 'identificador', $i);
+                                    if ($bus[$i + 7] !== ' ') {
+                                        /*    array_push($datosAlmacenados, $bus[$i + 7]); */
+                                        $datosAlmacenados[] = array($bus[$i + 7], 'identificador', $i);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            $operator = null;
         }
+        /*  */
     }
-    return $data;
+    return $datosAlmacenados;
 }
