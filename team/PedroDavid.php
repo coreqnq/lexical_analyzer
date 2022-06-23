09b3e558316c28a28fb8fb71c1bff95a67f18350
Avaricia <94759980+AvariciaTZ@users.noreply.github.com>
2022-06-22 21:17:39 -0500

    PD-update terminado
    PD-update terminado

--- team/PedroDavid.php
+++ team/PedroDavid.php
@@ -1,79 +1,32 @@
+
 <?php
 
-/* $evaluar = " _nag _num 56 _nag {}";
-/* _nag _num 56 _nag {} then if => <";
-var_dump(Identificador($evaluar)); */
-/* $evaluar = " _nag _num 56 _nag {}"; */
 
-/* $evaluar = " _nag = ( 2 + 4 ) / 3 if ( _Numero > 10 ) then ' Ingresaste '";
-var_dump(Identificador($evaluar)); */
-/* darle un espacio al final */
+/*$evaluar = " _Num = ( 2 + 4 ) / 3 if ( _Num > 10 ) then ' Ingresaste ' ";
+
+
+var_dump(Identificador($evaluar));
+
+*/
+
+/* Lógica */
 function Identificador($evaluar)
 {
-    $mayusculas =  ['_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    $minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    $data = array();
 
-    $operators = array_merge($minusculas, $mayusculas);
+    /* conjunto x conjunto */
+    $buscado = explode(" ", $evaluar);
 
-    /* analisame todo el array que debe coincidir por letra */
-    /*  $bus = array(); $bus=[]; */
-    $bus = array();
-    $bus = str_split($evaluar);
-
-    $alm =  array();
-    $alm[] = preg_replace("/[[:space:]]/", "", trim($evaluar));
-
-    $datosAlmacenados =  array();
-
-    /*    for ($i = 0; $i < count($operators); $i++) {
-    for ($i = 0; $i < 12; $i++) { */
-    for ($i = 0; $i < count($operators); $i++) {
-
-        if ($bus[$i] == '_') {
-            array_push($datosAlmacenados, "<br/>" . $bus[$i]);
-            if ($bus[$i + 1] !== ' ') {
-                array_push($datosAlmacenados, $bus[$i + 1]);
-                if ($bus[$i + 2] !== ' ') {
-                    array_push($datosAlmacenados, $bus[$i + 2]);
-                    if ($bus[$i + 3] !== ' ') {
-                        array_push($datosAlmacenados, $bus[$i + 3]);
-                        if ($bus[$i + 4] !== ' ') {
-                            array_push($datosAlmacenados, $bus[$i + 4]);
-                            if ($bus[$i + 5] !== ' ') {
-                                array_push($datosAlmacenados, $bus[$i + 5]);
-                                if ($bus[$i + 6] !== ' ') {
-                                    array_push($datosAlmacenados, $bus[$i + 6]);
-                                    if ($bus[$i + 7] !== ' ') {
-                                        array_push($datosAlmacenados, $bus[$i + 7]);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } elseif ($bus[$i] == null) {
-            break;
+    foreach ($buscado as $key => $value) {
+        //_nag
+        if ($value[0] == '$') {
+            /*  var_dump($value); */
+            /*  array_push($data, $value); */
+            $data[] = array($value, "Identificador ", $key);
         }
-        /*  */
     }
-    /*  */
 
 
-    /*  */
-    $arr = "";
-    $data =  array();
-    $key = array();
-    foreach ($datosAlmacenados as $key => $caracter) {
-
-        $arr .= $caracter;
-    }
-    if ($key == true) {
-        $data[] = array($arr, "Identificador ", $key);
-    } /* else {
-        $data[] = array("No hay alguna variable", "No especificado ", -1);
-    } */
-
-
+    /*  var_dump($data); */
     return $data;
 }
