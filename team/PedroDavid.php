153f5d4a83173f0ce59062191c4d0366eb990a29
Avaricia <94759980+AvariciaTZ@users.noreply.github.com>
2022-06-24 13:05:27 -0500

    PD-identificadores con digitos,enteros y reales
    Identificadores y datos numericos terminados.

--- team/PedroDavid.php
+++ team/PedroDavid.php
@@ -6,9 +6,10 @@
 var_dump(Identificador($evaluar));
 */
 
-/* Lógica */
+/* Lógica 1 */
 function Identificador($evaluar)
 {
+    /* LOGICA variables */
     $evaluar = trim($evaluar);
     $data = array();
     /* conjunto x conjunto */
@@ -22,6 +23,97 @@ function Identificador($evaluar)
                 /*  var_dump($value); */
                 /*  array_push($data, $value); */
                 $data[] = array($value, "Identificador ", $key);
+            }
+        }
+    }
+    /* LOGICA digitos,enteros y reales */
+    $numeros = array();
+
+    $numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $separador = '.';
+    $space = [' '];
+
+    foreach ($buscado as $key => $value) {
+
+        if ($value != null) {
+
+            if (array_key_exists($value[0], $numeros) == true) {
+
+                /* Número Real */
+                $contieneUnPunto = strpos($value, $separador);
+
+                if ($contieneUnPunto == true) {
+                    $data[] = array($value, "Número Real", $key);
+                } else {
+                    /* Número Entero o Dígito */
+                    if (isset($value[1])) {
+                        if ($value[1] != '.') {
+
+                            if (array_key_exists($value[1], $numeros) == true) {
+                                $data[] = array($value, "Número Entero", $key);
+                            }
+                        }
+                    } else {
+
+                        $data[] = array($value, "Dígito", $key);
+                    }
+                }
+
+
+                /*  $data[] = array($value, "Dígito", $key); */
+            }
+        }
+    }
+
+
+    return $data;
+}
+
+/* X: LOGICA digitos,enteros y reales */
+function Real($evaluar)
+{
+
+    $numeros = array();
+
+    $numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $separador = '.';
+    $space = [' '];
+
+    $evaluar = trim($evaluar);
+
+    $data = array();
+
+    $buscado = explode(" ", $evaluar);
+
+
+    foreach ($buscado as $key => $value) {
+
+        if ($value != null) {
+
+            if (array_key_exists($value[0], $numeros) == true) {
+
+                /* Número Real */
+                $contieneUnPunto = strpos($value, $separador);
+
+                if ($contieneUnPunto == true) {
+                    $data[] = array($value, "Número Real", $key);
+                } else {
+                    /* Número Entero o Dígito */
+                    if (isset($value[1])) {
+                        if ($value[1] != '.') {
+
+                            if (array_key_exists($value[1], $numeros) == true) {
+                                $data[] = array($value, "Número Entero", $key);
+                            }
+                        }
+                    } else {
+
+                        $data[] = array($value, "Dígito", $key);
+                    }
+                }
+
+
+                /*  $data[] = array($value, "Dígito", $key); */
             }
         }
     }
